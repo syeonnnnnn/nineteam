4993327aad506820aa8c9bcaa4a7af115318f91f
syeonnnnnn <ghdtj00@korea.ac.kr>
2023-08-18 03:51:30 +0900

    bug fix

--- config/urls.py
+++ config/urls.py
@@ -19,6 +19,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('account/', include('accountapp.urls')),
+    path('accounts/', include('accountapp.urls')),
     path('ebooks/', include('ebooksapp.urls'))
 ]
