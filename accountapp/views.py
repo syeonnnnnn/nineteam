df7d1bfc84e6e921ba616548c84308591dc11592
syeonnnnnn <ghdtj00@korea.ac.kr>
2023-08-18 04:35:35 +0900

    decorator

--- accountapp/views.py
+++ accountapp/views.py
@@ -1,15 +1,18 @@
+from django.contrib.auth.decorators import login_required
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from django.http import HttpResponse
 from django.shortcuts import render
 from django.urls import reverse_lazy
+from django.utils.decorators import method_decorator
 from django.views.generic import CreateView, DetailView, UpdateView, DeleteView
 
+from accountapp.decorators import account_ownership_required
 from accountapp.forms import AccountUpdateForm
 
 
 # Create your views here.
-
+has_ownership = [account_ownership_required, login_required]
 
 class AccountCreateView(CreateView):
     model = User
@@ -22,6 +25,9 @@ class AccountDetailView(DetailView):
     context_object_name = 'target_user'
     template_name = 'accountapp/detail.html'
 
+
+@method_decorator(has_ownership, 'get')
+@method_decorator(has_ownership, 'post')
 class AccountUpdateView(UpdateView):
     model = User
     context_object_name = 'target_user'
@@ -29,6 +35,9 @@ class AccountUpdateView(UpdateView):
     success_url = reverse_lazy('ebooksapp:ebooks')
     template_name = 'accountapp/update.html'
 
+
+@method_decorator(has_ownership, 'get')
+@method_decorator(has_ownership, 'post')
 class AccountDeleteView(DeleteView):
     model = User
     context_object_name = 'target_user'
