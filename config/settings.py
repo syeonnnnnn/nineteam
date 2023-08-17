3dbb12b371dddea5605e655533a932ce2cd67e89
syeonnnnnn <ghdtj00@korea.ac.kr>
2023-08-18 03:13:58 +0900

    login view

--- config/settings.py
+++ config/settings.py
@@ -15,6 +15,8 @@ from pathlib import Path
 import environ
 import os
 
+from django.urls import reverse_lazy
+
 env = environ.Env(
     # set casting, default value
     DEBUG=(bool, False)
@@ -144,3 +146,6 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_REDIRECT_URL = reverse_lazy('ebooksapp:ebooks')
+LOGOUT_REDIRECT_URL = reverse_lazy('accountapp:login')
